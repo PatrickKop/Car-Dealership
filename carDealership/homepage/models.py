9cc7cf7e44bb1a889a7bf622401879611346cda7
Patrick <koppatrick@yahoo.com>
2024-06-16 14:15:05 -0400

    added more tables to the database

--- carDealership/homepage/models.py
+++ carDealership/homepage/models.py
@@ -4,3 +4,17 @@ from django.db import models
 class cars(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=50)
+
+class customers(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField                           #look up more features for email
+
+class employees(models.Model):
+    employeeID = models.IntegerField                    #need to add way to incremnet and assign automatically
+    employeeFirstName = models.CharField(max_length=50)
+    employeeLastName = models.CharField(max_length=50)
+    employeeEmail = models.EmailField                   #look up more features for email
+
+class inventory(models.Model):
+    
