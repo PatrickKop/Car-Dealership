4259f0a046e6e0c7ba0e3e63129ed10f3a86fc1c
Patrick <koppatrick@yahoo.com>
2024-08-04 12:32:41 -0400

    Update

--- carDealership/homepage/views.py
+++ carDealership/homepage/views.py
@@ -207,7 +207,7 @@ def add_customer(request):
 
 def customer_record(request, pk):
     if request.user.is_authenticated:       #This is to make sure the user is logged in before viewing record
-        #Look up employee records
+        #Look up customer records
         customer_record = customer.objects.get(id=pk)      #Get will only get the object that is called. All will grab all the records
         return render(request, "customerRecord.html", {"customer_record":customer_record})
     else:
